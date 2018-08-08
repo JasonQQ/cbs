13c921f8c47e7a7b58bbfac3751986c724cac734
Jason <22829273@qq.com>
2018-08-08 20:21:17 +0800

    XssFilter support

---
+++ src/main/java/com/bluecity/cbs/system/config/XssConfig.java
@@ -0,0 +1,29 @@
+package com.bluecity.cbs.system.config;
+
+import com.bluecity.cbs.system.filter.XssFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class XssConfig {
+    /**
+     * xss过滤拦截器
+     */
+    @Bean
+    public FilterRegistrationBean<XssFilter> xssFilterRegistrationBean() {
+        FilterRegistrationBean<XssFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new XssFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.setEnabled(false);
+        filterRegistrationBean.addUrlPatterns("/*");
+        Map<String, String> initParameters = new HashMap<>();
+        initParameters.put("excludes", "/favicon.ico,/img/*,/js/*,/css/*");
+        initParameters.put("isIncludeRichText", "true");
+        filterRegistrationBean.setInitParameters(initParameters);
+        return filterRegistrationBean;
+    }
+}
